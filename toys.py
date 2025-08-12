a3b67d41a59ede9e57dc3bb8e27acd77dd8beac1
Ola Ipaye <ipayeola@outlook.com>
2025-08-12 23:18:09 +0100

    added 2 new toys to the toy box list in the toys.py file.

--- toys.py
+++ toys.py
@@ -4,12 +4,20 @@ toy_box = [
     {"name": "Race Car", "type": "Vehicle", "color": "Red"},
     {"name": "Lego Set", "type": "Blocks", "color": "Multi"},
     {"name": "Action Figure", "type": "Doll", "color": "Blue"},
-    {"name": "Teddy Bear", "type": "Stuffed Animal", "color": "Brown"}  # duplicate row
+    {"name": "Teddy Bear", "type": "Stuffed Animal", "color": "Brown"},  # duplicate row
 ]
 
-# Print all toys = all row of data
+# Adds 2 more toys to the toy box
+new_toys = [{"name": "Puzzles", "type": "Creative", "color": "Multi"},
+    {"name": "Toy Soldier", "type": "Action Figures", "color": "Black"}]
+
+toy_box.extend(new_toys)
+
+# Prints all toys = all row of data
 print("Here's what's in my toy box")
 for toy in toy_box:
-    print(f"{toy["name"]}, {toy["type"]}, {toy["color"]}")
+    print(f'{toy["name"]}, {toy["type"]}, {toy["color"]}')
 
+# Prints out number of toys in the box
 print("\nNumber of toys:", len(toy_box))
+
